d8c4131a1afa9e04b875d376b460fa0e208fb4c9
smolleya24 <smolleya24@gmail.com>
2023-04-24 10:36:49 -0400

    4/24/23 - 10:36 am

--- app/src/main/java/com/example/selfly/MainFragment.kt
+++ app/src/main/java/com/example/selfly/MainFragment.kt
@@ -5,15 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.selfly.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        binding.tipsVector.setOnClickListener {
+            rootView.findNavController()
+                .navigate(R.id.action_mainFragment_to_chooseTipFragment)
+        }
+
+        binding.moodVector.setOnClickListener {
+            rootView.findNavController()
+                .navigate(R.id.action_mainFragment_to_moodTrackerFragment)
+        }
+
+        binding.journalVector.setOnClickListener {
+            rootView.findNavController()
+                .navigate(R.id.action_mainFragment_to_chooseJournalFragment)
+        }
+
+        binding.calmVector.setOnClickListener {
+            rootView.findNavController()
+                .navigate(R.id.action_mainFragment_to_calmingDownFragment)
+        }
+        return rootView
     }
 
 }
