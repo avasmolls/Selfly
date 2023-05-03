ff28ce60c3eb571ca9c90d1a088cde1f5a596eae
smolleya24 <smolleya24@gmail.com>
2023-05-03 10:35:30 -0400

    5/3/23 - 10:35 am

--- app/src/main/java/com/example/selfly/CalmingDownFragment.kt
+++ app/src/main/java/com/example/selfly/CalmingDownFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.selfly.databinding.FragmentCalmingDownBinding
+import com.example.selfly.databinding.FragmentChooseJournalBinding
 
 class CalmingDownFragment : Fragment() {
+
+    private var _binding: FragmentCalmingDownBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calming_down, container, false)
+        _binding = FragmentCalmingDownBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        binding.circle.setOnClickListener {
+
+            binding.circle.animate().apply {
+                duration = 1000
+                rotationYBy(360f)
+                rotationXBy(360f)
+            }.start()
+
+        }
+
+        return rootView
     }
 
 }
