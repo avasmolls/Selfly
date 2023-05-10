89774a1cd50dfff723c46d4be8b8c8e9bb46c4c0
smolleya24 <smolleya24@gmail.com>
2023-05-10 10:35:26 -0400

    5/10/23 - 10:36 am

--- app/src/main/java/com/example/selfly/EntryViewHolder.kt
+++ app/src/main/java/com/example/selfly/EntryViewHolder.kt
@@ -1,11 +1,19 @@
 package com.example.selfly
 
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.selfly.databinding.ListItemLayoutJournalBinding
 
 class EntryViewHolder(val binding: ListItemLayoutJournalBinding) : RecyclerView.ViewHolder(binding.root) {
 
     private lateinit var currentEntry: Entry
+
+    init {
+        binding.root.setOnClickListener { view ->
+            val action = ChooseJournalFragmentDirections.actionChooseJournalFragmentToJournalWritingFragment(currentEntry.title, currentEntry.entryText)
+            view.findNavController().navigate(action)
+        }
+    }
 
     fun bindEntry(entry: Entry) {
         currentEntry = entry
